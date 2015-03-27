3539294133a6a66ce908a1342e6f197b5da9b7a8
Darren Cauthon <darren@cauthon.com>
2015-03-27 10:40:25 -0500

    Spacing.

--- lib/poxa_assist/rest_api.rb
+++ lib/poxa_assist/rest_api.rb
@@ -37,14 +37,14 @@ module PoxaAssist
 
       def pull_options_from_config_if_necessary options
         return options if options
-          {
-            host:       PoxaAssist.config.host,
-            ssl_port:   PoxaAssist.config.ssl_port,
-            port:       PoxaAssist.config.port,
-            app_id:     PoxaAssist.config.app_id,
-            app_key:    PoxaAssist.config.app_key,
-            app_secret: PoxaAssist.config.app_secret
-          }
+        {
+          host:       PoxaAssist.config.host,
+          ssl_port:   PoxaAssist.config.ssl_port,
+          port:       PoxaAssist.config.port,
+          app_id:     PoxaAssist.config.app_id,
+          app_key:    PoxaAssist.config.app_key,
+          app_secret: PoxaAssist.config.app_secret
+        }
       end
 
     end
