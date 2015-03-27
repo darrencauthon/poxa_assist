89132cfbc3c3f46a32e44d68ecf781948970e0cd
Darren Cauthon <darren@cauthon.com>
2015-03-27 09:57:28 -0500

    Refactor a bit.

--- lib/poxa_assist.rb
+++ lib/poxa_assist.rb
@@ -41,14 +41,18 @@ module PoxaAssist
 
   def self.config
     @options ||= {}
-    Struct.new(:host, :port, :ssl_port, :app_id, :app_key, :app_secret, :encrypted)
-          .new((@options[:host]       || ENV['POXA_HOST']),
-               (@options[:port]       || ENV['POXA_PORT']).to_i,
-               (@options[:ssl_port]   || ENV['POXA_SSL_PORT']).to_i,
-               (@options[:app_id]     || ENV['POXA_APP_ID']),
-               (@options[:app_key]    || ENV['POXA_APP_KEY']),
-               (@options[:app_secret] || ENV['POXA_APP_SECRET']),
-               (@options[:ssl_port]   || ENV['POXA_SSL_PORT']).to_i != 0)
+    val = {
+            host:       (@options[:host]       || ENV['POXA_HOST']),
+            port:       (@options[:port]       || ENV['POXA_PORT']).to_i,
+            ssl_port:   (@options[:ssl_port]   || ENV['POXA_SSL_PORT']).to_i,
+            app_id:     (@options[:app_id]     || ENV['POXA_APP_ID']),
+            app_key:    (@options[:app_key]    || ENV['POXA_APP_KEY']),
+            app_secret: (@options[:app_secret] || ENV['POXA_APP_SECRET']),
+          }
+    val[:encrypted] = val[:ssl_port] != 0
+
+    Struct.new(:host,      :port,      :ssl_port,      :app_id,      :app_key,      :app_secret,      :encrypted)
+          .new(val[:host], val[:port], val[:ssl_port], val[:app_id], val[:app_key], val[:app_secret], val[:encrypted])
   end
 
   # you will want to replace this yourself
